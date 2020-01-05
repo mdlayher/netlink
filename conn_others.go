b6309534a27a9209c0571187c3150318828aedbf
Alexis PIRES <alexis.pires@atos.net>
2020-01-05 22:40:15 +0100

    add unix import

--- conn_others.go
+++ conn_others.go
@@ -5,6 +5,8 @@ package netlink
 import (
 	"fmt"
 	"runtime"
+
+	"golang.org/x/sys/unix"
 )
 
 // errUnimplemented is returned by all functions on platforms that
@@ -23,8 +25,8 @@ type conn struct{}
 func dial(_ int, _ *Config) (*conn, uint32, error) { return nil, 0, errUnimplemented }
 func newError(_ int) error                         { return errUnimplemented }
 
-func (c *conn) Send(_ Message) error           { return errUnimplemented }
-func (c *conn) SendMessages(_ []Message) error { return errUnimplemented }
-func (c *conn) Receive() ([]Message, error)    { return nil, errUnimplemented }
-func (c *conn) Close() error                   { return errUnimplemented }
+func (c *conn) Send(_ Message) error                { return errUnimplemented }
+func (c *conn) SendMessages(_ []Message) error      { return errUnimplemented }
+func (c *conn) Receive() ([]Message, error)         { return nil, errUnimplemented }
+func (c *conn) Close() error                        { return errUnimplemented }
 func (c *conn) Select(_ *unix.Timeval) (int, error) { return -1, errUnimplemented }
