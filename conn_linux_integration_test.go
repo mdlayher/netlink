c7748d4df7c4ce706ce9037ec103c10fca8e6c62
Matt Layher <mdlayher@gmail.com>
2021-02-10 16:21:36 -0500

    netlink: ensure tests run on Go 1.12, add 1.12 CI build
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- conn_linux_integration_test.go
+++ conn_linux_integration_test.go
@@ -3,7 +3,6 @@
 package netlink_test
 
 import (
-	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -17,7 +16,6 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/jsimonetti/rtnetlink"
 	"github.com/jsimonetti/rtnetlink/rtnl"
-	"github.com/mdlayher/ethtool"
 	"github.com/mdlayher/netlink"
 	"github.com/mdlayher/netlink/nlenc"
 	"golang.org/x/net/bpf"
@@ -669,48 +667,6 @@ func TestIntegrationConnExecuteAfterReadDeadline(t *testing.T) {
 		t.Fatalf("Execute succeeded: got %v", got)
 	}
 	mustBeTimeoutNetError(t, err)
-}
-
-func TestIntegrationEthtoolExtendedAcknowledge(t *testing.T) {
-	t.Parallel()
-
-	// The ethtool package uses extended acknowledgements and should populate
-	// all of netlink.OpError's fields when unwrapped.
-	c, err := ethtool.New()
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			t.Skip("skipping, ethtool genetlink not available on this system")
-		}
-
-		t.Fatalf("failed to open ethtool genetlink: %v", err)
-	}
-
-	_, err = c.LinkInfo(ethtool.Interface{Name: "notexist0"})
-	if err == nil {
-		t.Fatal("expected an error, but none occurred")
-	}
-
-	var oerr *netlink.OpError
-	if !errors.As(err, &oerr) {
-		t.Fatalf("expected wrapped *netlink.OpError, but got: %T", err)
-	}
-
-	// Assume the message contents will be relatively static but don't hardcode
-	// offset just in case things change.
-	if oerr.Offset == 0 {
-		t.Fatal("no offset specified in *netlink.OpError")
-	}
-	oerr.Offset = 0
-
-	want := &netlink.OpError{
-		Op:      "receive",
-		Err:     unix.ENODEV,
-		Message: "no device matches name",
-	}
-
-	if diff := cmp.Diff(want, oerr); diff != "" {
-		t.Fatalf("unexpected *netlink.OpError (-want +got):\n%s", diff)
-	}
 }
 
 func TestIntegrationRTNetlinkStrictCheckExtendedAcknowledge(t *testing.T) {
--- netlink_linux_gteq_1.13_test.go
+++ netlink_linux_gteq_1.13_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/mdlayher/ethtool"
+	"github.com/mdlayher/netlink"
 	"golang.org/x/sys/unix"
 )
 
@@ -81,5 +83,47 @@ func TestOpErrorUnwrapLinux(t *testing.T) {
 				t.Fatalf("unexpected result (-want +got):\n%s", diff)
 			}
 		})
+	}
+}
+
+func TestIntegrationEthtoolExtendedAcknowledge(t *testing.T) {
+	t.Parallel()
+
+	// The ethtool package uses extended acknowledgements and should populate
+	// all of netlink.OpError's fields when unwrapped.
+	c, err := ethtool.New()
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			t.Skip("skipping, ethtool genetlink not available on this system")
+		}
+
+		t.Fatalf("failed to open ethtool genetlink: %v", err)
+	}
+
+	_, err = c.LinkInfo(ethtool.Interface{Name: "notexist0"})
+	if err == nil {
+		t.Fatal("expected an error, but none occurred")
+	}
+
+	var oerr *netlink.OpError
+	if !errors.As(err, &oerr) {
+		t.Fatalf("expected wrapped *netlink.OpError, but got: %T", err)
+	}
+
+	// Assume the message contents will be relatively static but don't hardcode
+	// offset just in case things change.
+	if oerr.Offset == 0 {
+		t.Fatal("no offset specified in *netlink.OpError")
+	}
+	oerr.Offset = 0
+
+	want := &netlink.OpError{
+		Op:      "receive",
+		Err:     unix.ENODEV,
+		Message: "no device matches name",
+	}
+
+	if diff := cmp.Diff(want, oerr); diff != "" {
+		t.Fatalf("unexpected *netlink.OpError (-want +got):\n%s", diff)
 	}
 }
