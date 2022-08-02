49941fc32339c198ed9fac832a29bc701c5dd181
Matt Layher <mdlayher@gmail.com>
2022-08-02 16:38:50 -0400

    *: Go 1.19
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- doc.go
+++ doc.go
@@ -5,16 +5,14 @@
 // Gophers Slack (https://invite.slack.golangbridge.org) in the #networking
 // channel!
 //
-//
-// Network namespaces
+// # Network namespaces
 //
 // This package is aware of Linux network namespaces, and can enter different
 // network namespaces either implicitly or explicitly, depending on
 // configuration. The Config structure passed to Dial to create a Conn controls
 // these behaviors. See the documentation of Config.NetNS for details.
 //
-//
-// Debugging
+// # Debugging
 //
 // This package supports rudimentary netlink connection debugging support. To
 // enable this, run your binary with the NLDEBUG environment variable set.
@@ -22,14 +20,14 @@
 //
 // To use the debugging defaults, use:
 //
-//   $ NLDEBUG=1 ./nlctl
+//	$ NLDEBUG=1 ./nlctl
 //
 // To configure individual aspects of the debugger, pass key/value options such
 // as:
 //
-//   $ NLDEBUG=level=1 ./nlctl
+//	$ NLDEBUG=level=1 ./nlctl
 //
 // Available key/value debugger options include:
 //
-//   level=N: specify the debugging level (only "1" is currently supported)
+//	level=N: specify the debugging level (only "1" is currently supported)
 package netlink
