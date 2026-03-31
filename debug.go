5c0c409fd9c6abadfe140f952a3c37eb05f05108
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2026-03-31 22:09:47 +0200

    debug: option to print using mnl format (#219)
    nftables uses the libmnl library that has a particular debugging format.
    This format is also used for nftables --debug=all.
    google/nftables golang library to interact with nftables uses this
    library to interface with netlink directly.
    Since these libraries are not well documented, is common to have to dump
    the netlink bits to compare.
    
    This commit implements an option to choose the same format used in
    nftables by adding a new option to the NLDEBUG environmenta variable and
    using the format=mnl key value.
    
    For Linux systems mnl is the default format.
    
    Change-Id: I5f63753b3ada4d25565fe0bddac35e010510a34f

--- debug.go
+++ debug.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -23,47 +22,12 @@ func init() {
 
 // A debugger is used to provide debugging information about a netlink connection.
 type debugger struct {
-	Log   *log.Logger
-	Level int
+	Log    *log.Logger
+	Level  int
+	Format string
 }
 
-// newDebugger creates a debugger by parsing key=value arguments.
-func newDebugger(args []string) *debugger {
-	d := &debugger{
-		Log:   log.New(os.Stderr, "nl: ", 0),
-		Level: 1,
-	}
-
-	for _, a := range args {
-		kv := strings.Split(a, "=")
-		if len(kv) != 2 {
-			// Ignore malformed pairs and assume callers wants defaults.
-			continue
-		}
-
-		switch kv[0] {
-		// Select the log level for the debugger.
-		case "level":
-			level, err := strconv.Atoi(kv[1])
-			if err != nil {
-				panicf("netlink: invalid NLDEBUG level: %q", a)
-			}
-
-			d.Level = level
-		}
-	}
-
-	return d
-}
-
-// debugf prints debugging information at the specified level, if d.Level is
-// high enough to print the message.
-func (d *debugger) debugf(level int, format string, v ...interface{}) {
-	if d.Level >= level {
-		d.Log.Printf(format, v...)
-	}
-}
-
+// panicf is a helper to panic with formatted text.
 func panicf(format string, a ...interface{}) {
 	panic(fmt.Sprintf(format, a...))
 }
